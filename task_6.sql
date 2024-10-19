025d3d743ea2b78e1d13ceb0f14b522d1a1499e0
blessingebele <blessmart.com@gmail.com>
2024-10-19 21:18:57 +0100

    file intgration

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,10 @@
 -- -- author: Anochili Blessing Ebele
 -- -- purpose:  Write a script that inserts multiple rows in the table customer (database alx_book_store) in your MySQL server.
 -- date: 14/10/2024
+
+-- Insert multiple rows into the Customers table
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES 
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
