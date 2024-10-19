0e9df9780192a09802e0535aff9778400df96186
blessingebele <blessmart.com@gmail.com>
2024-10-19 21:13:44 +0100

    file intgration

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,7 @@
 -- -- author: Anochili Blessing Ebele
 -- -- purpose: Write a script that inserts a single row in the table customer (database alx_book_store) in your MySQL server 
 -- date: 14/10/2024
+
+-- Insert a single row into the Customers table
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
