946b26a95b7ab714e473db16bc2db8fb7916f33b
blessingebele <blessmart.com@gmail.com>
2024-10-19 22:49:35 +0100

    g

--- pip.py
+++ pip.py
@@ -1,1 +1,23 @@
-pip.py
+#pip.py
+import mysql.connector
+mydb = mysql.connetor.connet(
+    host = "localhost",
+    user = "newuser",
+    password = "newuserpassword"
+)
+
+mycursor = mydb.cursor()
+
+
+mycursor.execute("CREATE DATABASE library")
+mycursor.execute("USE library")
+mycursor.execute("""
+    CREATE TABLE books
+                 id INT AUTO_INCREMENT PRIMARY KEY
+                 title VARCHAR(200)
+                 author VARCHAR(100)
+                 );
+""")
+result = mycursor.fetchall()
+for row in result:
+    print(row)
