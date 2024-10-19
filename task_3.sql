7149e28b44ca4daa27e755d4d80ca4b34b9035e7
blessingebele <blessmart.com@gmail.com>
2024-10-19 21:04:35 +0100

    file intgration

--- task_3.sql
+++ task_3.sql
@@ -1,3 +1,6 @@
 -- -- author: Anochili Blessing Ebele
 -- -- purpose:Write a script that list all the tables in alx_book_store in your MySQL server 
 -- date: 14/10/2024
+
+-- List all the tables in alx_book_store
+SHOW TABLES;
